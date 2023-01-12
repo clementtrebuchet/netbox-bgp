c10dc1591e98adaca26427ec8bbc751ad833dbb4
clement.trebuchet <clement.trebuchet.external@airbus.com>
2023-01-12 20:02:19 +0400

    Refactored Black

--- netbox_bgp/graphql.py
+++ netbox_bgp/graphql.py
@@ -1,37 +1,35 @@
-from graphene import ObjectType, Field
+from graphene import ObjectType
 
-from netbox.graphql.scalars import BigInt
-from netbox.graphql.types import NetBoxObjectType
 from netbox.graphql.fields import ObjectField, ObjectListField
-
+from netbox.graphql.types import NetBoxObjectType
 from . import models, filters
 
 
 class CommunityType(NetBoxObjectType):
     class Meta:
         model = models.Community
-        fields = '__all__'
+        fields = "__all__"
         filterset_class = filters.CommunityFilterSet
 
 
 class BgpSessionType(NetBoxObjectType):
     class Meta:
         model = models.BGPSession
-        fields = '__all__'
+        fields = "__all__"
         filterset_class = filters.BGPSessionFilterSet
 
 
 class PeerGroupType(NetBoxObjectType):
     class Meta:
         model = models.BGPPeerGroup
-        fields = '__all__'
+        fields = "__all__"
         filterset_class = filters.BGPPeerGroupFilterSet
 
 
 class RoutingPolicyType(NetBoxObjectType):
     class Meta:
         model = models.RoutingPolicy
-        fields = '__all__'
+        fields = "__all__"
         filterset_class = filters.RoutingPolicyFilterSet
 
 
